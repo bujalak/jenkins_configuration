62ad2c2d95b1f0c33cce8c96de14459cde241856
user <user@DESKTOP-TE0J58I>
2025-10-25 18:54:05 +0530

    added changes in runner file

--- src/test/java/CucumberOptions/TestRunner.java
+++ src/test/java/CucumberOptions/TestRunner.java
@@ -4,7 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/Features", glue="Step_Definitions", monochrome = true,
-plugin = {"pretty", "html:target/cucumber.html","json:target/cucumber2.json"}, tags="@smoke"
+plugin = {"pretty",  "html:target/cucumber-reports/html",
+        "json:target/cucumber-reports/json/cucumber.json"
+        }
 		)
 public class TestRunner extends AbstractTestNGCucumberTests {
 	

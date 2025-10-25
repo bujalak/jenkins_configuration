809ef0bea4dd6eb144124bd697b17976901ee97d
user <user@DESKTOP-TE0J58I>
2025-10-25 19:16:20 +0530

    added reports plugin

--- src/test/java/CucumberOptions/TestRunner.java
+++ src/test/java/CucumberOptions/TestRunner.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/Features", glue="Step_Definitions", monochrome = true,
-plugin = {"pretty",  "html:target/cucumber-reports/html",
-        "json:target/cucumber-reports/json/cucumber.json"
+plugin = {"pretty",  "html:test-output/html-report",
+        "json:test-output/json-report/cucumber.json"
         }
 		)
 public class TestRunner extends AbstractTestNGCucumberTests {
